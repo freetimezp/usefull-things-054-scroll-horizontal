4f7475b45efacf8bb622f72f298c8f1f90b3c39d
freetimezp <kraftukrnet@ukr.net>
2025-03-03 22:19:05 +0200

    02. usefull-things-054-scroll-horizontal. UI. Horizontal scroll.

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+var blocks = document.getElementsByClassName("block");
+var container = document.getElementsByClassName("container");
+var hs = new HorizontalScroll.default({
+    blocks: blocks,
+    container: container,
+    isAnimated: true,
+    springEffect: 0.8
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
